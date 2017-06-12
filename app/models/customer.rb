8aa271253f54dfcc2b5b9f02295c6087cf629268
Uwe Kubosch <uwe@datek.no>
2017-06-12 21:13:03 +0200

    Customer name need not be unique

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,8 +5,8 @@ class Customer < ApplicationRecord
 
   before_validation { self.email = email.downcase.strip if email_changed? }
 
-  validates :email, :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :address, :birthdate, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :address, :birthdate, :name, presence: true
 
   def image=(file)
     # self.filename = file.original_filename
