c9696a5a5bee5992c450518319e88d7bbb916013
Uwe Kubosch <uwe@datek.no>
2017-06-14 08:48:22 +0200

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.action_mailer.default_url_options = { host: 'houseofnerds.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'houseofnerds.herokuapp.com', protocol: 'https' }
   config.action_mailer.perform_caching = false
   # config.action_mailer.raise_delivery_errors = false
   # config.active_job.queue_adapter     = :resque
@@ -22,7 +22,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
   config.consider_all_requests_local = false
   config.eager_load = true
-  # config.force_ssl = true
+  config.force_ssl = true
   config.i18n.fallbacks = true
   config.log_formatter = ::Logger::Formatter.new
   config.log_level = :debug
