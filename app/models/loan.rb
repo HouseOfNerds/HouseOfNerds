9eb11c6c8a19ee0f9febd421bb940cb8c7d4ca66
Uwe Kubosch <uwe@datek.no>
2018-01-14 21:17:08 +0100

    Avoid loading images in the indexes

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -6,15 +6,15 @@ class Loan < ApplicationRecord
 
   validates :customer_id, :asset_type_id, presence: true
 
-  scope :today, -> { where('created_at >= :today', today: Time.zone.now.beginning_of_day) }
+  scope :today, -> { where('loans.created_at >= :today', today: Time.zone.now.beginning_of_day) }
   scope :yesterday, -> {
-    where('created_at >= :yesterday AND created_at < :today',
+    where('loans.created_at >= :yesterday AND loans.created_at < :today',
         yesterday: Time.zone.now.beginning_of_day - 1.day, today: Time.zone.now.beginning_of_day)
   }
   scope :outstanding, -> { where(returned_at: nil) }
   scope :returned, -> { where.not(returned_at: nil) }
   scope :today_or_outstanding, -> { today.or(outstanding) }
   scope :older, -> {
-    where('created_at < :today', today: Time.zone.now.beginning_of_day)
+    where('loans.created_at < :today', today: Time.zone.now.beginning_of_day)
   }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-Administrator.where(email: 'uwe@kubosch.no').first_or_create!(name: 'Uwe Kubosch')
+oslo = Department.create! name: 'Oslo', domain: 'houseofnerds.no'
+Administrator.where(email: 'uwe@kubosch.no').first_or_create!(name: 'Uwe Kubosch', department: oslo)
