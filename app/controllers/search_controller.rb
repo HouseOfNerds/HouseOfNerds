f88a77ebfb86928757f4fe1eb0cbb269ed2e1bb6
Uwe Kubosch <uwe@datek.no>
2018-01-14 22:40:51 +0100

    Avoid loading images for search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
     @query = params[:q]&.strip
     return if @query.blank?
     db_query = "%#{@query}%"
-    @customers = Customer
+    @customers = Customer.without_images
         .where('email ILIKE :query OR name ILIKE :query OR phone ILIKE :query',
             query: db_query)
         .order(updated_at: :desc)
