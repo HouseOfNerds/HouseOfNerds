627fac10054d2c8a5c7384052e6cfc429f026b4c
Uwe Kubosch <uwe@datek.no>
2018-02-04 12:01:47 +0100

    debug ci coverage

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+puts 'loading department model'
+
 class Department < ApplicationRecord
   validates :name, :domain, presence: true, uniqueness: { case_sensitive: true }
 end
