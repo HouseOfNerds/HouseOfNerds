eb11797178ec3fe88cac91a76bb773564014b3c8
Uwe Kubosch <uwe@datek.no>
2018-02-04 12:03:50 +0100

    Avoid loading models for coverage during db:setup

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-puts 'loading department model'
-
 class Department < ApplicationRecord
   validates :name, :domain, presence: true, uniqueness: { case_sensitive: true }
 end
